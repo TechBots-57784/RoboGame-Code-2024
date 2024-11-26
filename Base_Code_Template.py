cf46278bd19aaf42f695a0948847b11c913107fb
vmylavarapu <vamsimylavarapu@gmail.com>
2024-11-26 09:59:32 -0600

    Update Base_Code_Template.py

--- Base_Code_Template.py
+++ Base_Code_Template.py
@@ -1,16 +1,17 @@
 from hub import motion_sensor, port
-import runloop, motor_pair, motor,runloop 
-
+import runloop, motor_pair, motor,runloop
+import time
+import sys
 # vars and constants
 WHEEL_CIRCUMFERENCE=17.5 # 27.6 is the circumference of ADB large wheel and 17.5 is the circumference of ADB small wheel
 DEFAULT_SPEED=650 # Small motor (essential): -660 to 660 Medium motor: -1110 to 1110 Large motor: -1050 to 1050
 # ports
-MOVEMENT_MOTOR_RIGHT=port.A
-MOVEMENT_MOTOR_LEFT=port.B
-COLOR_SENSOR_RIGHT=port.C
-COLOR_SENSOR_LEFT=port.D
-EXTENSION_MOTOR_TOP=port.E
-EXTENSION_MOTOR_BOTTOM=port.F
+MOVEMENT_MOTOR_RIGHT=port.F #red
+MOVEMENT_MOTOR_LEFT=port.B #yellow
+COLOR_SENSOR_RIGHT=port.D # no color
+COLOR_SENSOR_LEFT=port.C # red and yellow
+EXTENSION_MOTOR_TOP=port.E #blue
+EXTENSION_MOTOR_BOTTOM=port.A #green
 
 # prep the hub
 motion_sensor.reset_yaw(0)
@@ -19,7 +20,7 @@ motor_pair.pair(motor_pair.PAIR_1,MOVEMENT_MOTOR_LEFT,MOVEMENT_MOTOR_RIGHT)
 
 # drive with a certain speed
 async def drive(distance, speed=DEFAULT_SPEED):
-    rotation_in_degrees = round( ( distance / WHEEL_CIRCUMFERENCE ) * 360 ) 
+    rotation_in_degrees = round( ( distance / WHEEL_CIRCUMFERENCE ) * 360 )
     await motor_pair.move_for_degrees(motor_pair.PAIR_1, rotation_in_degrees, 0, velocity = speed, stop=motor.SMART_COAST)
 
 # drive in an arc with variying motor speeds for left and right
@@ -48,7 +49,14 @@ async def moveMotor(side, degrees, speed=DEFAULT_SPEED):
     if (side == "bottom"):
         await motor.run_for_degrees(EXTENSION_MOTOR_BOTTOM, degrees, speed, stop = motor.HOLD)
 
-# main code 
+async def resetExtension(extension=EXTENSION_MOTOR_TOP):
+    if motor.relative_position(extension) <= 180:
+        resetDirection=motor.SHORTEST_PATH
+    else:
+        resetDirection=motor.LONGEST_PATH
+    await motor.run_to_absolute_position(extension,0,720,direction=resetDirection,stop=motor.HOLD)
+
+# main code
 async def main():
     '''
     await drive(50) # drives forward 50 cms straight with DEFAULT_SPEED
@@ -69,7 +77,6 @@ async def main():
 ###################################################################################
 
 
-
 ###################################################################################
 ###################################################################################
 # DO NOT CHANGE ANYTHING AFTER THIS LINE. WRITE MISSION CODE BEFORE THIS LINE
@@ -77,3 +84,4 @@ async def main():
 ###################################################################################
 
 runloop.run(main())
+sys.exit()
