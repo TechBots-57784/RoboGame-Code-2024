441a4be64c92af5b80509ee03c6dee49631f42ad
vmylavarapu <vamsimylavarapu@gmail.com>
2024-11-26 13:37:38 -0600

    Update 3.py

--- REGIONALS/3.py
+++ REGIONALS/3.py
@@ -1,5 +1,4 @@
-# 06 - Raise the Mast
-# 03 - Flip coral reef
-# 01 - Flip coral buds
-# 02 - Release shark
-# 04 - deliver Scuba Diver
+# 02 - deliver shark
+# 08 - Artificial habitat
+# collect Krill
+# collect part of trident
