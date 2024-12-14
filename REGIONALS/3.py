c155b64d98101d847f9268187e52957d02ef51e2
rishi-tsalla <147455110+rishi-tsalla@users.noreply.github.com>
2024-12-14 11:07:08 -0500

    Update 3.py

--- REGIONALS/3.py
+++ REGIONALS/3.py
@@ -180,12 +180,7 @@ async def main():
 
     # flip trident
     await drive(-10,1050)
-    await moveMotor('drop','top',140)
-    await turnLeft(20)
-
-    # go to right and also solve octopus on the way
-    await moveMotor('lift','top',330)
-    await turnRight(100)
+    await turnRight(70)
     await drive(83,1050)
     await turnLeft(125)
     await drive(36,1050)
